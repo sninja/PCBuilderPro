9774e92f4ed2c5174456ff68860901be65a0394f
sumit <ninjasumit5@gmail.com>
2022-09-19 15:44:03 +0530

    added CrossOrigin annotation

--- PCBuilderPro/src/main/java/com/cdac/controller/CustomerController.java
+++ PCBuilderPro/src/main/java/com/cdac/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.cdac.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import com.cdac.service.FeedbackService;
 import com.cdac.service.OrderService;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class CustomerController {
 	
 	@Autowired
