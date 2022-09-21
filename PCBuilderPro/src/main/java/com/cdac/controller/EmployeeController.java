a05d5bdacc8d3b63cfa9278538310db8fe8a3404
sumit <ninjasumit5@gmail.com>
2022-09-21 16:13:11 +0530

    testing annotation

--- PCBuilderPro/src/main/java/com/cdac/controller/EmployeeController.java
+++ PCBuilderPro/src/main/java/com/cdac/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.cdac.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.cdac.service.FeedbackService;
 import com.cdac.service.OrderService;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class EmployeeController {
 	
 	@Autowired
