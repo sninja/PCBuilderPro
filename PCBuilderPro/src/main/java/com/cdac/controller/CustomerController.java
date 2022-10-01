444b7e7c572068c98cab7ad0193ddd9f89b2f595
sumit <ninjasumit5@gmail.com>
2022-10-01 12:53:05 +0530

    prevent repetition of items in cart

--- PCBuilderPro/src/main/java/com/cdac/controller/CustomerController.java
+++ PCBuilderPro/src/main/java/com/cdac/controller/CustomerController.java
@@ -36,94 +36,94 @@ import com.cdac.service.OrderService;
 @CrossOrigin("http://localhost:3000/")
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@Autowired
 	private ComponentService componentService;
-	
 
 	@Autowired
 	private FeedbackService feedbackService;
-	
+
 	@Autowired
 	private CartRepository cartRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	List<Component> temp = new ArrayList();
-	
+
 	/*
 	 * @PostMapping("/register") public String addCustomer(@RequestBody Customer
 	 * customer) { try { customerService.save(customer); return
 	 * "Registration successful"; } catch(CustomerServiceException e){ return
 	 * e.getMessage(); } }
 	 */
-	
+
 	@ResponseBody
 	@GetMapping("/customerOrders/{email}")
 	public List<Order> getAllOrders(@PathVariable String email) {
 		User user = userRepository.findByEmail(email);
-		//System.out.println("IDDDDDDDDDDDDDDDDDDDDDDDDDD ==== " + user.getId());
+		// System.out.println("IDDDDDDDDDDDDDDDDDDDDDDDDDD ==== " + user.getId());
 		return customerService.fetchOrder(user.getId());
 	}
-	
+
 	@ResponseBody
 	@GetMapping("/userComponents")
-	public List<Component> getAllComponents(){
+	public List<Component> getAllComponents() {
 		return componentService.fetchAllComponents();
 	}
-	
-	//add to cart table
+
+	// add to cart table
 	@PostMapping("/addCart/{id}")
 	public String addtoCart(@PathVariable int id) {
-		 Component component = componentService.fetchbyId(id);
-		 Cart cart = new Cart();
-		 cart.setName(component.getName());
-		 cart.setPrice(component.getPrice());
-		 cartRepository.save(cart);
-		 
-		 temp.add(component);  
-		return " Added to cart Successfully";
+		Component component = componentService.fetchbyId(id);
+		Cart cart = new Cart();
+		cart.setName(component.getName());
+		cart.setPrice(component.getPrice());
+		if (!cartRepository.existsByName(component.getName())) {
+			cartRepository.save(cart);
+
+			temp.add(component);
+			return " Added to cart Successfully";
+		}
+		return "Component already in cart";
+
 	}
-	
-	//delete from cart
+
+	// delete from cart
 	@DeleteMapping("/deleteItem/{id}")
 	public String deleteItem(@PathVariable int id) {
 		cartRepository.deleteById(id);
 		return "Item deleted successfully";
-	} 
-	
-	
+	}
+
 	@PostMapping("/addOrder/{email}")
 	public String addOrder(@PathVariable String email) {
-		
+
 		/* Generating random transaction ID */
 		int leftLimit = 48; // numeral '0'
-	    int rightLimit = 122; // letter 'z'
-	    int targetStringLength = 10;
-	    Random random = new Random();
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 10;
+		Random random = new Random();
 
-	    String generatedString = random.ints(leftLimit, rightLimit + 1)
-	      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-	      .limit(targetStringLength)
-	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-	      .toString();
-	//========================================	
+		String generatedString = random.ints(leftLimit, rightLimit + 1)
+				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+		// ========================================
 		User user = userRepository.findByEmail(email);
 		List<Cart> cart = (List<Cart>) cartRepository.findAll();
 		float bill = cartRepository.fetchBill();
-		//creating bill
+		// creating bill
 		Bill billob = new Bill();
 		billob.setStatus("Paid");
 		billob.setAmount(bill);
-		
-		//creating order
+
+		// creating order
 		Order order = new Order();
 		order.setStatus("Processing");
 		order.setTrasactionId(generatedString);
@@ -133,41 +133,40 @@ public class CustomerController {
 		componentService.stockUpdate(temp);
 		billob.setOrder(order);
 		orderService.save(order);
-		//temp.clear();
+		// temp.clear();
 		cartRepository.deleteAll();
 		return "order Added Successfully";
 	}
-	
+
 	@GetMapping("/getCart")
-	public List<Cart> getCartItems(){
+	public List<Cart> getCartItems() {
 		return (List<Cart>) cartRepository.findAll();
 	}
-	
+
 	@PostMapping("/clearCart")
 	public void deleteCart() {
 		cartRepository.deleteAll();
 	}
-	
-	
+
 	@GetMapping("/getCartNumber")
 	public long countCartItems() {
 		return cartRepository.count();
 	}
-	
+
 	@GetMapping("/getBill")
 	public float getBilldata() {
 		return cartRepository.fetchBill();
 	}
-		
+
 	@PostMapping("/addFeedback/{id}")
 	public String addFeedback(@PathVariable int id, @RequestBody Feedback feedback) {
 		feedbackService.save(id, feedback);
 		return "feedback Added Successfully";
 	}
-	
+
 	@ResponseBody
 	@GetMapping("/orderComponents/{id}")
 	public List<Component> fetchOrderComponents(@PathVariable int id) {
-	    return componentService.fetchOrderComponents(id);
+		return componentService.fetchOrderComponents(id);
 	}
 }
